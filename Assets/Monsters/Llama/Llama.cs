eb529761d0b45c43f8056a3f4305706e8939ac83
Tom <tom.marx@hotmail.fr>
2016-11-12 13:47:41 +0100

    Début animé lama

--- Assets/Monsters/Llama/Llama.cs
+++ Assets/Monsters/Llama/Llama.cs
@@ -4,11 +4,35 @@ using System.Collections;
 public class Llama : MonoBehaviour {
 	public GameObject prefab;
 	public float targetSpeed, accelerationDuration, fireRate, fireSpeed, noFireArea;
+	Animator anim;
+	SpriteRenderer sprite;
 	float destX, destY;
 	float speed {
 		get;
 		set;
 	}
+	int direction {
+		get {
+			if ((body.velocity.x > 0 && body.velocity.x > body.velocity.y) || (body.velocity.x < 0 && body.velocity.x < body.velocity.y)) {
+				if (body.velocity.x > 0)
+					return 2;
+				if (body.velocity.x < 0)
+					return 1;
+			}
+			else {
+				if (body.velocity.y > 0)
+					return 3;
+				if (body.velocity.y < 0)
+					return 0;
+			}
+			return 0;
+		}
+
+		set	{
+			direction = value;
+		}
+	}
+
 	float timeBefore;
 	Rigidbody2D body;
 
@@ -19,6 +43,8 @@ public class Llama : MonoBehaviour {
 		timeBefore = Time.time;
 		//Acceleration
 		this.floatTo("speed", accelerationDuration, targetSpeed, false);
+		sprite = gameObject.FindChildByName ("Sprite").GetComponent<SpriteRenderer> ();
+		anim = gameObject.FindChildByName("Sprite").GetComponent<Animator> ();
 	}
 
 	void FixedUpdate() {
@@ -27,16 +53,33 @@ public class Llama : MonoBehaviour {
 
 		body.velocity = (new Vector3(destX, destY, 0) - transform.position).normalized * speed;
 
+		if (direction == 2)
+			sprite.flipX = true;
+	
+		if (direction == 1)
+			sprite.flipX = false;
+
+
 		//Tire quand le firerate est dépassé
 		if (Time.time - timeBefore >= fireRate) {
 			if (Vector3.Distance(new Vector3(destX, destY, 0), transform.position) > noFireArea) {
-				GameObject go = (GameObject)Instantiate(prefab);
-				go.transform.position = transform.position;
-                // Son de tirs
-                go.GetComponent<Sputum> ().Fire (destX, destY, fireSpeed);
+
+				anim.SetBool ("Sputum", true);
 				timeBefore = Time.time;
+				this.WaitAndDo (1.5f, Shoot);
+
 			}
 		}
+		anim.SetInteger ("Direction", direction);
+		Debug.Log (anim.GetBool("Sputum"));
+	}
+
+	void Shoot () {
+		GameObject go = (GameObject)Instantiate(prefab);
+		go.transform.position = transform.position;
+		// Son de tirs
+		go.GetComponent<Sputum> ().Fire (destX, destY, fireSpeed);
+		//anim.SetBool ("Sputum", false);
 	}
 
 }
