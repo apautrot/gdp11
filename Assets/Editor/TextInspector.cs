4aa422d1e56b2a2416b2e4f7c4b25ba61bee4077
apautrot <alexis@pautrot.com>
2016-11-13 08:48:53 +0100

    1ère version Game Over screen

---
+++ Assets/Editor/TextInspector.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor ( typeof ( TextMeshEx ) ), CanEditMultipleObjects]
+public class TextInspector : Editor
+{
+	[MenuItem ( "GameObject/Create Other/3D Text (extended)", false, 5 )]
+	public static void CreateTextMeshEx ()
+	{
+		GameObject go = new GameObject();
+		go.name = "Text";
+
+		TextMeshEx textMesh = go.GetOrCreateComponent<TextMeshEx> ();
+		textMesh.Font = Resources.GetBuiltinResource ( typeof ( Font ), "Arial.ttf" ) as Font;
+
+		if ( Selection.activeGameObject != null )
+		{
+			go.transform.parent = Selection.activeGameObject.transform;
+			go.transform.position = Selection.activeGameObject.transform.position;
+		}
+		Selection.activeObject = go;
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		this.DrawDefaultInspector ();
+
+		UnityToolbag.SortingLayerExposedEditor.DrawSortingLayerGUI ( target, targets );
+
+		// http://twistedoakstudios.com/blog/Post373_unity-font-extraction
+	}
+}
--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -1,10 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class GameOver : MonoBehaviour
 {
+	GameObject blackFade;
+
+	void Awake ()
+	{
+		blackFade = gameObject.FindChildByName ( "Black Fade" );
+		blackFade.SetActive ( false );
+	}
+
 	void Start ()
 	{
-		transform.scaleFrom ( 0.5f, 0 );
+		gameObject.FadeIn ();
+	}
+
+	void Update ()
+	{
+		if ( InputConfiguration.Instance.ActionA.IsJustUp )
+		{
+			blackFade.SetActive ( true );
+			blackFade.SetAlpha ( 0 );
+			blackFade.FadeIn ( 1 ).setOnCompleteHandler ( c => SceneManager.LoadScene ( "Test Scene Alexis" ) );
+		}
 	}
 }
