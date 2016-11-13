b2a7538dab454e8d1451278b4048b05d925c5079
apautrot <alexis@pautrot.com>
2016-11-13 12:49:25 +0100

    Changements sur chenille

--- Assets/Monsters/Caterpillar/SectionCaterpillar.cs
+++ Assets/Monsters/Caterpillar/SectionCaterpillar.cs
@@ -3,19 +3,11 @@ using System.Collections;
 
 public class SectionCaterpillar : MonoBehaviour
 {
-    public float speed;
-    Rigidbody2D body;
-	public GameObject following;
+	GameObject following;
 
-    void Awake()
+    internal void SetParentSection ( GameObject parent )
     {
-        speed = 50;
-        body = GetComponent<Rigidbody2D>();
-    }
-
-    void FixedUpdate()
-    {
-        body.velocity = (following.transform.position - transform.position).normalized * speed;
-
-    }
+		following = parent;
+		GetComponent <Joint2D> ().connectedBody = parent.GetComponent<Rigidbody2D> ();
+	}
 }
